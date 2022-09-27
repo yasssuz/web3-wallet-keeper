513427a3deea062272cf63ae4c0fdd686e16b1c4
Karim Yassine <karimwebdevelopment@gmail.com>
2022-09-26 22:07:43 -0300

    feat: 🎸 create header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,73 @@
+// react or any other library related content
+import { useState } from "react";
+
+// styles
+import { HeaderContainer, InteractionArea, LogoArea } from "./styles";
+
+// components
+import CreateOrImportWalletModalState from "../modal-states/create-import-wallet";
+import RecoverWallet from "../modal-states/recover-wallet";
+import Button from "../shared/buttons";
+import Modal from "../shared/modal";
+import Heading from "../shared/typography/heading";
+
+// services and utils
+
+// images
+
+// types
+
+// interfaces
+interface HeaderProps {
+  isSidebarOpen: boolean;
+  currentAddress: string | undefined;
+}
+
+function Header({ isSidebarOpen, currentAddress }: HeaderProps) {
+  const [isCreateWalletModalOpen, setIsCreateWalletModalOpen] =
+      useState<boolean>(false),
+    [isRecoverWalletModalOpen, setIsRecoverWalletModalOpen] =
+      useState<boolean>(false);
+
+  return (
+    <HeaderContainer>
+      <LogoArea isSidebarOpen={isSidebarOpen}></LogoArea>
+      <InteractionArea>
+        <Heading as='h1' size='large'>
+          {currentAddress}
+        </Heading>
+        <Button
+          variant='primary'
+          size='large'
+          width='184px'
+          type='button'
+          onClick={() => setIsCreateWalletModalOpen(true)}
+        >
+          + Create new wallet
+        </Button>
+        {currentAddress && (
+          <Button
+            variant='tertiary'
+            size='large'
+            type='button'
+            width='fit-content'
+            onClick={() => setIsRecoverWalletModalOpen(true)}
+          >
+            Reveal Secret Recovery Phrase
+          </Button>
+        )}
+        <Modal isOpen={isCreateWalletModalOpen}>
+          <CreateOrImportWalletModalState
+            create
+            setIsModalOpen={setIsCreateWalletModalOpen}
+          />
+        </Modal>
+        <Modal isOpen={isRecoverWalletModalOpen}>
+          <RecoverWallet setIsModalOpen={setIsRecoverWalletModalOpen} />
+        </Modal>
+      </InteractionArea>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
---
+++ src/components/header/styles.ts
@@ -0,0 +1,111 @@
+import styled from "@emotion/styled";
+import { MediaQuery } from "../../utils/global";
+
+export const HeaderContainer = styled.header`
+  border-bottom: 1px solid var(--line);
+  display: grid;
+  grid-template-columns: min-content 1fr;
+  background: var(--secondaryBg);
+  transition: height 0.3s ease, background 0.25s cubic-bezier(0.53, 0.21, 0, 1),
+    border-bottom 0.25s cubic-bezier(0.53, 0.21, 0, 1);
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  z-index: 10;
+  height: 96px;
+
+  ${MediaQuery.md} {
+    height: 81px;
+  }
+
+  ${MediaQuery.sm} {
+    height: 64px;
+  }
+`;
+
+export const LogoArea = styled.div<{ isSidebarOpen: boolean }>`
+  border-right: 1px solid var(--line);
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding-left: 32px;
+  width: ${props => (props.isSidebarOpen ? "300px" : "209px")};
+  transition: width ${props => (props.isSidebarOpen ? "0.3s" : "0.5s")}
+      cubic-bezier(0.53, 0.21, 0, 1)
+      ${props => (props.isSidebarOpen ? "0.27s" : "0s")},
+    padding-left 0.3s cubic-bezier(0.53, 0.21, 0, 1)
+      ${props => (props.isSidebarOpen ? "0.27s" : "0s")},
+    border-right 0.25s cubic-bezier(0.53, 0.21, 0, 1);
+
+  ${MediaQuery.md} {
+    width: ${props => (props.isSidebarOpen ? "261px" : "200px")};
+    padding-left: 24px;
+  }
+
+  ${MediaQuery.sm} {
+    width: 56px;
+    padding-left: 16px;
+    border-right: 0;
+  }
+`;
+
+export const LogoDesktop = styled.svg`
+  path {
+    transition: fill 0.25s cubic-bezier(0.53, 0.21, 0, 1);
+  }
+
+  ${MediaQuery.sm} {
+    display: none;
+  }
+`;
+
+export const LogoMobile = styled.img`
+  display: none;
+
+  ${MediaQuery.sm} {
+    display: block;
+  }
+`;
+
+export const InteractionArea = styled.div`
+  padding: 0 32px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  gap: 16px;
+
+  h1 {
+    margin-right: auto;
+  }
+
+  ${MediaQuery.md} {
+    padding: 0 24px;
+  }
+
+  ${MediaQuery.sm} {
+    padding: 0 16px 0 0;
+
+    button:first-of-type {
+      display: none;
+    }
+
+    button:nth-of-type(2) {
+      display: grid;
+      height: 32px;
+    }
+  }
+`;
+
+export const SettingsButton = styled.button`
+  margin-left: 24px;
+  line-height: 0;
+  border: 0;
+  background: none;
+  cursor: pointer;
+
+  ${MediaQuery.sm} {
+    margin-left: 16px;
+  }
+`;
